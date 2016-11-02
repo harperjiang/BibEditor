4c4c7a8605fe387eabe7c9668f177aeca9f2faba
Harper <harperjiang@msn.com>
2016-11-02 18:27:02 -0500

    Build alpha1

--- BibEditorPlugin/src/edu/uchicago/cs/hao/bibeditor/Activator.java
+++ BibEditorPlugin/src/edu/uchicago/cs/hao/bibeditor/Activator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) Oct 2016 Hao Jiang.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Hao Jiang - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
 package edu.uchicago.cs.hao.bibeditor;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
