0b43c1bcebf3dbf31cbc6499ad1472f3a284da96
Harper Jiang <harperjiang@msn.com>
2016-11-04 23:49:00 -0500

    Daily Update Site

---
+++ BibEditorUpdateSite/update_site.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+USITE_REPO=/home/harper/Repositories/BibEditor/BibEditorUpdateSite
+WEBSITE_REPO=/home/harper/Repositories/websites/personal/bibeditor/updatesite
+
+rm -r $WEBSITE_REPO/features
+rm -r $WEBSITE_REPO/plugins
+rm -r $WEBSITE_REPO/artifacts.jar
+rm -r $WEBSITE_REPO/content.jar
+
+cp -r $USITE_REPO/features $WEBSITE_REPO/
+cp -r $USITE_REPO/plugins $WEBSITE_REPO/
+cp -r $USITE_REPO/artifacts.jar $WEBSITE_REPO/
+cp -r $USITE_REPO/content.jar $WEBSITE_REPO/
+
+cd $WEBSITE_REPO
+git add -A .
+git commit -m "Daily Update Site"
+git push origin master
+
+ssh hajiang@people.cs.uchicago.edu -t ". ~/update_site.sh"
