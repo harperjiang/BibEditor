0760a1c4f39f75808a7a733f7eb808af998e3029
Hao Jiang <harperjiang@msn.com>
2017-09-25 02:31:31 -0500

    Update TeXDojo

--- TeXDojoUpdateSite/update_site.sh
+++ TeXDojoUpdateSite/update_site.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
-#USITE_REPO=/home/harper/Repositories/BibEditor/BibEditorUpdateSite
-#WEBSITE_REPO=/home/harper/Repositories/websites/personal/WebContent/bibeditor/updatesite
+USITE_REPO=/home/harper/git/TeXDojo/TeXDojoUpdateSite
+WEBSITE_REPO=/home/harper/git/website-personal/WebContent/texdojo/updatesite
 
-USITE_REPO=/home/Cathy/c/workspace/TeXDojo/TeXDojoUpdateSite
-WEBSITE_REPO=/home/Cathy/c/workspace/website-personal/WebContent/bibeditor/updatesite
+#USITE_REPO=/home/Cathy/c/workspace/TeXDojo/TeXDojoUpdateSite
+#WEBSITE_REPO=/home/Cathy/c/workspace/website-personal/WebContent/bibeditor/updatesite
+
+cd $WEBSITE_REPO
+git stash
+git pull
 
 rm -rf $WEBSITE_REPO/features
 rm -rf $WEBSITE_REPO/plugins
 rm -rf $WEBSITE_REPO/artifacts.jar
 rm -rf $WEBSITE_REPO/content.jar
+rm -rf $WEBSITE_REPO/index.html
+rm -rf $WEBSITE_REPO/site.xml
 
 cp -r $USITE_REPO/features $WEBSITE_REPO/
 cp -r $USITE_REPO/plugins $WEBSITE_REPO/
 cp -r $USITE_REPO/artifacts.jar $WEBSITE_REPO/
 cp -r $USITE_REPO/content.jar $WEBSITE_REPO/
+cp -r $USITE_REPO/index.html $WEBSITE_REPO/
+cp -r $USITE_REPO/site.xml $WEBSITE_REPO/
 
-cd $WEBSITE_REPO
-git stash
-git pull
 git add -A .
 git commit -m "Daily Update Site"
 git push origin master
 
-ssh hajiang@people.cs.uchicago.edu -t ". ~/update_site.sh"
+ssh hajiang@linux1.cs.uchicago.edu -t ". ~/update_site.sh"
